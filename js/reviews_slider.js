8ede1d9179f0274c58ecc587676fd87f89218bb4
santillo4ka <mesanova032@gmail.com>
2026-02-19 19:27:59 +0300

    слайдер

--- js/reviews_slider.js
+++ js/reviews_slider.js
@@ -1,54 +1,38 @@
 const reviewsButtonPrevEl = document.querySelector('.reviews_slick_prev');
 const reviewsButtonNextEl = document.querySelector('.reviews_slick_next');
-const viewportEl = document.querySelector('.reviews_viewport');
-const containerReviewsSliderEl = document.querySelector('.reviews_slider');
+const sliderViewportEl = document.querySelector('.reviews_slider');
 const reviewsItemEls = [...document.querySelectorAll('.reviews_item')];
 
-let currentIndex = 0;
+const getMaxScroll = () => {
+	return sliderViewportEl.scrollWidth - sliderViewportEl.clientWidth;
+};
 
-const getSliderData = () => {
-	const widthOfItem = reviewsItemEls[0].getBoundingClientRect().width;
-	const visibleItems = Math.round(viewportEl.offsetWidth / widthOfItem );
-	const maxIndex = reviewsItemEls.length - visibleItems;
-
-	return { widthOfItem, maxIndex };
-}
+const getItemWidth = () => {
+	return reviewsItemEls[0].offsetWidth;
+};
 
 const updateButtons = () => {
-	const { maxIndex } = getSliderData();
-	reviewsButtonPrevEl.classList.toggle('inactive', currentIndex === 0);
-	reviewsButtonNextEl.classList.toggle('inactive', currentIndex === maxIndex);
+	const maxScroll = getMaxScroll();
+	reviewsButtonPrevEl.classList.toggle('inactive', sliderViewportEl.scrollLeft <= 0);
+	reviewsButtonNextEl.classList.toggle('inactive', sliderViewportEl.scrollLeft >= maxScroll);
 }
 
 updateButtons();
 
-const updateSliderPosition = () => {
-	const { widthOfItem } = getSliderData();
-	containerReviewsSliderEl.style.transform = `translateX(-${currentIndex * widthOfItem}px)`;
-}
-
 reviewsButtonNextEl.addEventListener('click', () => {
-	const { maxIndex } = getSliderData();
-	if (currentIndex < maxIndex) {
-		currentIndex += 1;
-		updateButtons();
-		updateSliderPosition();
-	}
+	sliderViewportEl.scrollLeft += getItemWidth();
 });
 
 reviewsButtonPrevEl.addEventListener('click', () => {
-	if (currentIndex > 0) {
-		currentIndex -= 1;
-		updateButtons();
-		updateSliderPosition();
-	}
+	sliderViewportEl.scrollLeft -= getItemWidth();
 });
 
+sliderViewportEl.addEventListener('scroll', updateButtons);
 window.addEventListener('resize', () => {
-	const { maxIndex } = getSliderData();
-	if (currentIndex > maxIndex) {
-		currentIndex = maxIndex;
+	const maxScroll = getMaxScroll();
+	if (sliderViewportEl.scrollLeft > maxScroll) {
+		sliderViewportEl.scrollLeft = maxScroll;
 	}
+
 	updateButtons();
-	updateSliderPosition();
 });
