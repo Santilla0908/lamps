f89c081cc4ff725d56227e64b786d4651af2495b
santillo4ka <mesanova032@gmail.com>
2026-03-03 13:13:50 +0300

    всплывашка

--- js/modal-dialog.js
+++ js/modal-dialog.js
@@ -20,11 +20,23 @@
 		modalEl.close('success');
 	});
 
-	const showToast = () => {
+	const showToast = (duration = 3000) => {
+		let hideTimeout;
 		toastEl.classList.add('toast_show');
-		setTimeout(() => {
+
+		hideTimeout = setTimeout(() => {
 			toastEl.classList.remove('toast_show');
-		}, 3000);
+		}, duration);
+
+		toastEl.addEventListener('mouseenter', () => {
+			clearTimeout(hideTimeout);
+		});
+
+		toastEl.addEventListener('mouseleave', () => {
+			hideTimeout = setTimeout(() => {
+				toastEl.classList.remove('toast_show');
+			}, 1000);
+		});
 	}
 
 	modalEl.addEventListener('click', e => {
