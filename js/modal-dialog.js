2622dae6abb990f2b1f6f281fc0f35d90e1ab8f7
santillo4ka <mesanova032@gmail.com>
2026-03-03 18:56:45 +0300

    click

--- js/modal-dialog.js
+++ js/modal-dialog.js
@@ -2,7 +2,7 @@
 	const modalEl = document.querySelector('.dialog');
 	const openModalEls = document.querySelectorAll('.open_modal');
 	const closeModalEl = document.querySelector('.modal_close');
-	const submitBtnEl = document.querySelector('.submit_btn');
+	const submitBtnEl = document.querySelectorAll('.submit_btn');
 	const toastEl = document.querySelector('.toast');
 
 	openModalEls.forEach(button => {
@@ -16,28 +16,33 @@
 		modalEl.close();
 	});
 
-	submitBtnEl.addEventListener('click', e => {
-		modalEl.close('success');
+	let hideTimeout;
+
+	toastEl.addEventListener('mouseenter', () => {
+		clearTimeout(hideTimeout);
+	});
+
+	toastEl.addEventListener('mouseleave', () => {
+		hideTimeout = setTimeout(() => {
+			toastEl.classList.remove('toast_show');
+		}, 1000);
 	});
 
 	const showToast = (duration = 3000) => {
-		let hideTimeout;
 		toastEl.classList.add('toast_show');
-
+		clearTimeout(hideTimeout);
 		hideTimeout = setTimeout(() => {
 			toastEl.classList.remove('toast_show');
 		}, duration);
-
-		toastEl.addEventListener('mouseenter', () => {
-			clearTimeout(hideTimeout);
-		});
-
-		toastEl.addEventListener('mouseleave', () => {
-			hideTimeout = setTimeout(() => {
-				toastEl.classList.remove('toast_show');
-			}, 1000);
-		});
 	}
+
+	submitBtnEl.forEach(button => {
+		button.addEventListener('click', () => {
+			const dialog = button.closest('.dialog');
+			if (dialog) return modalEl.close('success');
+			showToast();
+		});
+	});
 
 	modalEl.addEventListener('click', e => {
 		const isOverlayClick = e.target === e.currentTarget;
