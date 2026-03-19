7dd3710ff3c54c1e51e2d07a69d0b9be3ed8e204
santillo4ka <mesanova032@gmail.com>
2026-03-19 21:13:53 +0300

    slider

--- js/advantages_slider.js
+++ js/advantages_slider.js
@@ -4,132 +4,142 @@
 	const sliderEl = document.querySelector('.advantages_slider');
 	const originalItemEls = [ ...document.querySelectorAll('.advantage_item') ];
 
-	const sliderState = {
-		currentIndex: 0,
-		itemWidth: 0,
-		visibleItems: 0,
-		cloneCount: 0,
-		isScrolling: false
-	}
+	let currentIndex = 0;
+	let itemWidth = 0;
+	let visibleItems = 0;
+	let cloneCount = 0;
+	let isScrolling = false;
 
 	const updateSliderState = () => {
-		const itemWidth = sliderEl.children[0].offsetWidth;
-		const visibleItems = Math.round(sliderEl.offsetWidth / itemWidth);
+		const newItemWidth = sliderEl.children[0].offsetWidth;
+		const newVisibleItems = Math.round(sliderEl.offsetWidth / newItemWidth);
 
-		sliderState.itemWidth = itemWidth;
-		sliderState.visibleItems = visibleItems;
-		sliderState.cloneCount = visibleItems;
+		itemWidth = newItemWidth;
+		visibleItems = newVisibleItems;
+		cloneCount = newVisibleItems;
 	}
 
 	const createClones = () => {
-
 		const originalLength = originalItemEls.length;
 
-		for (let i = originalLength - 1; i >= originalLength - sliderState.cloneCount; i--) {
+		for (let i = originalLength - 1; i >= originalLength - cloneCount; i--) {
 			const clone = originalItemEls[i].cloneNode(true);
 			sliderEl.prepend(clone);
 		}
-		for (let i = 0; i < sliderState.cloneCount; i++) {
+		for (let i = 0; i < cloneCount; i++) {
 			const clone = originalItemEls[i].cloneNode(true);
 			sliderEl.append(clone);
 		}
 	}
 
 	const setStartPosition = () => {
-		sliderState.currentIndex = sliderState.cloneCount;
-		sliderEl.scrollLeft = sliderState.itemWidth * sliderState.currentIndex;
+		currentIndex = cloneCount;
+		sliderEl.scrollLeft = itemWidth * currentIndex;
 	}
 
 	const checkClones = () => {
 		const originalLength = originalItemEls.length;
 
-		if (sliderState.currentIndex >= originalLength + sliderState.cloneCount) {
-			sliderState.currentIndex -= originalLength;
-			sliderEl.scrollLeft = sliderState.currentIndex * sliderState.itemWidth;
+		if (currentIndex >= originalLength + cloneCount) {
+			currentIndex -= originalLength;
+			sliderEl.scrollLeft = currentIndex * itemWidth;
 		}
 
-		if (sliderState.currentIndex < sliderState.cloneCount) {
-			sliderState.currentIndex += originalLength;
-			sliderEl.scrollLeft = sliderState.currentIndex * sliderState.itemWidth;
+		if (currentIndex < cloneCount) {
+			currentIndex += originalLength;
+			sliderEl.scrollLeft = currentIndex * itemWidth;
 		}
 	}
 
 	const prevSlide = () => {
-		if (sliderState.isScrolling) return;
-		sliderState.currentIndex -= 1;
-		sliderState.isScrolling = true;
+		if (isScrolling) return;
+		currentIndex -= 1;
+		isScrolling = true;
 
 		sliderEl.scrollTo({
-			left: sliderState.currentIndex * sliderState.itemWidth,
+			left: currentIndex * itemWidth,
 			behavior: "smooth",
 		});
+		setTimeout(() => {
+			checkClones();
+			isScrolling = false;
+		}, 300);
 	}
 
 	const nextSlide = () => {
-		if (sliderState.isScrolling) return;
-		sliderState.currentIndex += 1;
-		sliderState.isScrolling = true;
+		if (isScrolling) return;
+		currentIndex += 1;
+		isScrolling = true;
 
 		sliderEl.scrollTo({
-			left: sliderState.currentIndex * sliderState.itemWidth,
+			left: currentIndex * itemWidth,
 			behavior: "smooth",
 		});
+		setTimeout(() => {
+			checkClones();
+			isScrolling = false;
+		}, 300);
 	}
 
 	prevEl.addEventListener('click', prevSlide);
 	nextEl.addEventListener('click', nextSlide);
+	sliderEl.addEventListener('click', () => {
+		sliderEl.focus();
+	});
 
 	updateSliderState();
 	createClones();
 	setStartPosition();
 
-	let isDragging = false;
-	let startX = 0;
-	let startScrollLeft = 0;
+	const maxScrollLeft = sliderEl.scrollWidth - sliderEl.clientWidth;
+	const minScrollLeft = 0;
+	const resetScrollPosition = sliderEl.scrollLeft;
+	const reset2ScrollPosition = resetScrollPosition + ((originalItemEls.length - cloneCount) * itemWidth);
+	let scrollStartPos = resetScrollPosition;
+	let pointerStartX;
 
-	sliderEl.addEventListener('click', () => {
-		sliderEl.focus();
-	});
+	const mouseMoveHandler = e => {
+		const moveX = pointerStartX - e.pageX;
+		sliderEl.scrollLeft = (() => {
+			const toScroll = scrollStartPos + moveX;
+			if (moveX >= 0) {
+				if (toScroll <= maxScrollLeft) return toScroll;
+				const overflow = toScroll - maxScrollLeft;
+				return resetScrollPosition + overflow;
+			} else {
+				if (toScroll >= minScrollLeft) return toScroll;
+				const overflow = minScrollLeft - toScroll;
+				return reset2ScrollPosition - overflow;
+			}
+		})();
+	};
+
+	const mouseUpHandler = e => {
+		window.removeEventListener('mousemove', mouseMoveHandler);
+		window.removeEventListener('mouseup', mouseUpHandler);
+		window.removeEventListener('mouseleave', mouseUpHandler);
+
+		currentIndex = Math.round(sliderEl.scrollLeft / itemWidth);
+
+		sliderEl.scrollTo({
+			left: currentIndex * itemWidth,
+			behavior: "smooth"
+		});
+		setTimeout(() => {
+			checkClones();
+			isScrolling = false;
+		}, 300);
+	}
 
 	sliderEl.addEventListener('mousedown', e => {
 		e.preventDefault();
-		isDragging = true;
-		startX = e.clientX;
-		startScrollLeft = sliderEl.scrollLeft;
-		sliderEl.classList.add('drag');
-	});
 
-	sliderEl.addEventListener('mousemove', e => {
-		if (!isDragging) return;
-		const moveX = e.clientX - startX;
-		sliderEl.scrollLeft = startScrollLeft - moveX;
-		sliderState.currentIndex = sliderEl.scrollLeft / sliderState.itemWidth;
-	});
+		pointerStartX = e.pageX;
+		scrollStartPos = sliderEl.scrollLeft;
 
-	sliderEl.addEventListener('mouseup', () => {
-		if (!isDragging) return;
-		isDragging = false;
-		sliderEl.classList.remove('drag');
-
-		sliderState.currentIndex = Math.round(sliderEl.scrollLeft / sliderState.itemWidth);
-		checkClones();
-
-		sliderEl.scrollTo({
-			left: sliderState.currentIndex * sliderState.itemWidth,
-			behavior: "smooth"
-		});
-	});
-
-	sliderEl.addEventListener('mouseleave', () => {
-		isDragging = false;
-		sliderEl.classList.remove('drag');
-
-		sliderState.currentIndex = Math.round(sliderEl.scrollLeft / sliderState.itemWidth);
-
-		sliderEl.scrollTo({
-			left: sliderState.currentIndex * sliderState.itemWidth,
-			behavior: "smooth"
-		});
+		window.addEventListener('mousemove', mouseMoveHandler);
+		window.addEventListener('mouseup', mouseUpHandler);
+		window.addEventListener('mouseleave', mouseUpHandler);
 	});
 
 	sliderEl.addEventListener('keydown', e => {
@@ -148,16 +158,14 @@
 	const observer = new ResizeObserver(() => {
 		updateSliderState();
 		sliderEl.scrollTo({
-			left: sliderState.currentIndex * sliderState.itemWidth,
+			left: currentIndex * itemWidth,
 			behavior: 'auto'
 		});
+		setTimeout(() => {
+			checkClones();
+			isScrolling = false;
+		}, 300);
 	});
-
-	/*sliderEl.addEventListener('scrollend', () => {
-		sliderState.currentIndex = Math.round(sliderEl.scrollLeft / sliderState.itemWidth);
-		checkClones();
-		sliderState.isScrolling = false;
-	});*/
 
 	observer.observe(sliderEl);
 }
