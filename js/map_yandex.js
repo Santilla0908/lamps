eea7970b980fb4720bd101e7d52f276385042ddf
santillo4ka <mesanova032@gmail.com>
2026-05-19 18:48:46 +0300

    map

---
+++ js/map_yandex.js
@@ -0,0 +1,38 @@
+const mapElement = document.querySelector('.map');
+
+const initMap = () => {
+	const map = new ymaps.Map(mapElement, {
+		center: [55.840093, 37.516350],
+		zoom: 17,
+	});
+
+	let isMapActive = false;
+
+	map.behaviors.disable('scrollZoom');
+	mapElement.addEventListener('click', () => {
+		isMapActive = true;
+		map.behaviors.enable('scrollZoom');
+	});
+	mapElement.addEventListener('mouseleave', () => {
+		isMapActive = false;
+		map.behaviors.disable('scrollZoom');
+	});
+
+	const placemark = new ymaps.Placemark(
+		[55.840093, 37.516350],
+		{
+			balloonContent:
+				`<div class="map_balloon">
+					<h3 class="map_balloon_title">Главная лампочка</h3>
+					<p class="map_balloon_text">Купи здесь, купи сейчас!</p>
+				</div>`
+		}
+	);
+
+	map.geoObjects.add(placemark);
+	placemark.balloon.open();
+}
+
+ymaps.ready(initMap);
+
+
